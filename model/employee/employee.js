9631acc95e0a4e84ce6a7edf99796a07c2194d31
Programmer NBA <137274609+programmer-nba@users.noreply.github.com>
2024-02-20 22:05:35 +0700

    Update employee.js

--- model/employee/employee.js
+++ model/employee/employee.js
@@ -4,67 +4,67 @@ const Joi = require("joi");
 var bcrypt = require("bcrypt");
 
 const EmployeeSchema = new Schema({
-    employee_number: {type:Number, required: true},
-    userid:{type:String, require: true},
-    first_name: {type:String, require: true},
-    last_name: {type:String, require: true},
-    nick_name: {type:String, require: true},
-    iden_number: {type:String, require: true},
-    password: {type:String, require: true},
-    role:{
-        role: {type:String, require: false},
-        position: {type:String, require: false},
-        department: {type:String, require: false},
-        job_position: {type:String, require: false},
-    },
-    tel: {type:Number, require: true},
-    address: {type:String, require:true},
-    subdistrict: {type:String, require: true},
-    district: {type: String, required: true},
-    provice: {type: String, required: true},
-    postcode: {type: String, required: true},
-    birthday: {type: Date, required: true},
-    age: {type: Number, required: true},
-    email: {type: String, required: true},
-},{timestamps: true});
+  employee_number: { type: Number, required: true },
+  userid: { type: String, require: true },
+  first_name: { type: String, require: true },
+  last_name: { type: String, require: true },
+  nick_name: { type: String, require: true },
+  iden_number: { type: String, require: true },
+  password: { type: String, require: true },
+  role: {
+    role: { type: String, require: false },
+    position: { type: String, require: false },
+    department: { type: String, require: false },
+    job_position: { type: String, require: false },
+  },
+  tel: { type: Number, require: true },
+  address: { type: String, require: true },
+  subdistrict: { type: String, require: true },
+  district: { type: String, required: true },
+  provice: { type: String, required: true },
+  postcode: { type: String, required: true },
+  birthday: { type: Date, required: true },
+  age: { type: Number, required: true },
+  email: { type: String, required: true },
+}, { timestamps: true });
 
-EmployeeSchema.pre('save',function(next){   //ทำ Middleware การ Hash ก่อน EmployeeScheme ที่ User กรอกมาจะ save
+EmployeeSchema.pre('save', function (next) {   //ทำ Middleware การ Hash ก่อน EmployeeScheme ที่ User กรอกมาจะ save
   const user = this
   bcrypt.hash(user.password, 10).then(hash => {
-      user.password = hash
-      next()
-  }).catch(error =>{
-      console.error(error)
+    user.password = hash
+    next()
+  }).catch(error => {
+    console.error(error)
   })
 })
 
 const Employees = mongoose.model("employees", EmployeeSchema);
 
- const Validate = (data)=>{
-   const schema = Joi.object({
-        employee_number: Joi.number().required().label('กรุณากรอกไอดี'),
-        userid: Joi.string().required().label('กรุณากรอกยูสเซอร์ไอดี'),
-        first_name: Joi.string().required().label('กรุณากรอกชื่อจริง'),
-        last_name: Joi.string().required().label('กรุณากรอกนามสกุล'),
-        nick_name: Joi.string().required().label('กรุณากรอกชื่อเล่น'),
-        iden_number: Joi.string().required().label('กรุณากรอกบัตรประชาชน'),
-        password: Joi.string().required().label('กรุณากรอกรหัสผ่าน'),
-        role: Joi.string(),
-        position: Joi.string(),
-        department: Joi.string(),
-        job_position: Joi.string(),
-        role: Joi.string(),
-        tel: Joi.number().required().label('กรุณากรอกหมายเลขโทรศัพท์'),
-        address: Joi.string().required().label('กรุณากรอกที่อยู่'),
-        subdistrict: Joi.string().required().label('กรุณากรอกตำบล'),
-        district: Joi.string().required().label('กรุณากรอกอำเภอ'),
-        provice: Joi.string().required().label('กรุณากรอกจังหวัด'),
-        postcode: Joi.number().required().label('กรุณากรอกรหัสไปรษณีย์'),
-        birthday: Joi.date().required().label('กรุณากรอกวันเกิด'),
-        age: Joi.number().required().label('กรุณากรอกอายุ'),
-        email: Joi.string().required().label('กรุณากรอกอีเมล์'),
-   });
-   return schema.validate(data);
-  };
+const Validate = (data) => {
+  const schema = Joi.object({
+    employee_number: Joi.number().required().label('กรุณากรอกไอดี'),
+    userid: Joi.string().required().label('กรุณากรอกยูสเซอร์ไอดี'),
+    first_name: Joi.string().required().label('กรุณากรอกชื่อจริง'),
+    last_name: Joi.string().required().label('กรุณากรอกนามสกุล'),
+    nick_name: Joi.string().required().label('กรุณากรอกชื่อเล่น'),
+    iden_number: Joi.string().required().label('กรุณากรอกบัตรประชาชน'),
+    password: Joi.string().required().label('กรุณากรอกรหัสผ่าน'),
+    role: Joi.string(),
+    position: Joi.string(),
+    department: Joi.string(),
+    job_position: Joi.string(),
+    role: Joi.string(),
+    tel: Joi.number().required().label('กรุณากรอกหมายเลขโทรศัพท์'),
+    address: Joi.string().required().label('กรุณากรอกที่อยู่'),
+    subdistrict: Joi.string().required().label('กรุณากรอกตำบล'),
+    district: Joi.string().required().label('กรุณากรอกอำเภอ'),
+    provice: Joi.string().required().label('กรุณากรอกจังหวัด'),
+    postcode: Joi.number().required().label('กรุณากรอกรหัสไปรษณีย์'),
+    birthday: Joi.date().required().label('กรุณากรอกวันเกิด'),
+    age: Joi.number().required().label('กรุณากรอกอายุ'),
+    email: Joi.string().required().label('กรุณากรอกอีเมล์'),
+  });
+  return schema.validate(data);
+};
 
-module.exports = {Employees, Validate};
+module.exports = { Employees, Validate };
