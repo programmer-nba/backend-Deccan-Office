bead9100157602ae0c993358a235aac0c48e9a49
Bew2000 <146051593+Bew2000@users.noreply.github.com>
2024-03-29 16:05:27 +0700

    29/03/2024

--- model/employee/timeInOutEmployee.js
+++ model/employee/timeInOutEmployee.js
@@ -24,25 +24,6 @@ const timeSchema = new Schema({
     status_timeOut: {type:String, default: "false", require: false},
 },{timestamps:true});
 
-timeSchema.pre('save', function(next) {
-    //ตรวจสอบช่วงเวลาที่ทำการเริ่มงานเข้ามา
-    /*if(this.period == "morning"){
-        this.time_out = dayjs().set('hour', 12).set('minute', 0).set('second', 0).set('millisecond', 0).toDate();
-    } else if(this.period == "afternoon"){
-        this.time_out = dayjs().set('hour', 18).set('minute', 0).set('second', 0).set('millisecond', 0).toDate();
-    } else if(this.period == "day"){
-        this.time_out = dayjs().set('hour', 18).set('minute', 0).set('second', 0).set('millisecond', 0).toDate();
-    }*/
-
-    // เปลี่ยน Time Zone จาก UTC เป็น ประเทศไทย
-    //this.time_in = dayjs(this.time_in).tz(timeZone).toDate();
-    //this.time_out = dayjs(this.time_out).tz(timeZone).toDate();
-
-    //console.log(dayjs(this.time_in).format()); // แสดง time_in ว่าสามารถเปลี่ยน Time Zone ได้ไหม
-    //console.log(dayjs(this.time_out).format()); // แสดง time_out ทดสอบว่าสามารถเปลี่ยน Time Zone ได้ไหม
-    
-    next();
-});
 
 const timeInOut = mongoose.model("TimeInOut", timeSchema);
 
