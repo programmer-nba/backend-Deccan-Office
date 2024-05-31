eff1b4bd532fd6516025996b99f90bebd5a13028
KieKoongDev <kiekoong.dev@gmail.com>
2024-05-31 16:14:34 +0700

    aa

--- routes/Term/term.route.js
+++ routes/Term/term.route.js
@@ -5,12 +5,14 @@ const TermsController = require('../../controllers/Terms/terms.controller');
 router.post("/create", TermsController.create)
 router.put("/:id", TermsController.update)
 router.get("/all", TermsController.getAll)
-router.get("/:id", TermsController.getOne)
+router.get("/:id/one", TermsController.getOne)
 router.get("/:code/all", TermsController.getAllStandardByCode)
 router.get("/:code/one", TermsController.getOneStandardByCode)
 router.delete("/:id", TermsController.deleteOne)
 
 router.post("/accepted", TermsController.createAcceptedTerm)
-router.get("/accepted/all", TermsController.getAcceptedTerms)
+router.get("/accepteds", TermsController.getAcceptedTerms)
+
+router.get("/:id/userterms", TermsController.getUserAcceptedTerms)
 
 module.exports = router;
