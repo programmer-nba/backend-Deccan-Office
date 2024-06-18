7a51f3710de0c854f7f22c171cdea50a2e77d667
Programmer NBA <programmer@nbadigitalworlds.com>
2024-06-18 14:41:29 +0700

    up

--- index.js
+++ index.js
@@ -99,6 +99,9 @@ app.use(ddsc + '/event', require('./routes/event/event.route'))
 //tossgun
 app.use(ddsc + '/onestopservice', require('./routes/Tossagunshop/onestopservice'));
 
+// ผู้ลงทุน One Stop Shop
+app.use(ddsc + '/invest', require('./routes//Invest/invest'));
+
 
 const port = process.env.PORT || 9996;
 
